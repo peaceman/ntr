22e138502ef59ceb861f21773f9b45fa0d3b59bc
peaceman <nixalio@gmail.com>
2014-10-23 11:03:06 +0200

    start of the register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,20 @@
-trololo register here or so?
+@extends('layouts/default')
+@section('content')
+<div class="page-header">
+	<h3>Registration</h3>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<form class="form-horizontal">
+				<div class="form-group">
+					<label for="email" class="col-md-2 control-label">Email</label>
+					<div class="col-md-10">
+						<input type="email" name="email" class="form-control">
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+@stop
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,8 +4,14 @@
 	<meta charset="UTF-8">
 	<title>nTimeRec</title>
 	<link rel="stylesheet" href="{{ elixir('css/vendor.css') }}" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="{{ elixir('css/main.css') }}" media="screen" charset="utf-8">
 	<script src="{{ elixir('js/vendor.js') }}"></script>
 </head>
 <body>
+	@include('partials.navbar')
+
+	<div class="container">
+		@yield('content')
+	</div><!-- /.container -->
 </body>
 </html>
