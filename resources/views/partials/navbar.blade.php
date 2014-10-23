6158d277a3b960621837a7c8132efdf435269e6f
peaceman <nixalio@gmail.com>
2014-10-23 16:26:32 +0200

    add login, logout and register link to the navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,20 +1,35 @@
 <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-	<div class="container">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="#">nTimeRec</a>
-		</div>
-		<div class="collapse navbar-collapse">
-			<ul class="nav navbar-nav">
-				<li class="active"><a href="#">Home</a></li>
-				<li><a href="#about">About</a></li>
-				<li><a href="#contact">Contact</a></li>
-			</ul>
-		</div><!--/.nav-collapse -->
-	</div>
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">nTimeRec</a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                {{--<li class="active">--}}
+                    {{--<a href="#">Home</a>--}}
+                {{--</li>--}}
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+            @if(Auth::check())
+                <li>
+                    <a href="{{ route('auth.logout') }}">Logout</a>
+                </li>
+            @else
+                <li>
+                    <a href="{{ route('auth.register') }}">Register</a>
+                </li>
+                <li>
+                    <a href="{{ route('auth.login') }}">Login</a>
+                </li>
+            @endif
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
 </div>
