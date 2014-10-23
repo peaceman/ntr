da98f6aed2749a4fea2becda7b3731f3be74b457
peaceman <nixalio@gmail.com>
2014-10-23 09:46:18 +0200

    add default layout

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>nTimeRec</title>
+	<link rel="stylesheet" href="{{ elixir('css/vendor.css') }}" media="screen" charset="utf-8">
+	<script src="{{ elixir('js/vendor.js') }}"></script>
+</head>
+<body>
+</body>
+</html>
