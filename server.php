6ad3dbf252ddb486f8a85257d77d3740a2b725b4
peaceman <nixalio@gmail.com>
2014-10-23 08:59:34 +0200

    set public path in server.php

--- server.php
+++ server.php
@@ -3,10 +3,9 @@
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = urldecode($uri);
+$publicPath = __DIR__ . '/public';
 
-$paths = require __DIR__.'/bootstrap/paths.php';
-
-$requested = $paths['public'].$uri;
+$requested = $publicPath.$uri;
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
@@ -16,4 +15,4 @@ if ($uri !== '/' and file_exists($requested))
 	return false;
 }
 
-require_once $paths['public'].'/index.php';
+require_once $publicPath.'/index.php';
