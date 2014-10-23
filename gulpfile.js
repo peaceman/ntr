14b12618480d8834d7dd201779e03f4d7376274a
peaceman <nixalio@gmail.com>
2014-10-23 11:03:29 +0200

    integrate less stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ require('laravel-elixir-bower');
 
 elixir(function(mix) {
     mix.bower()
-       .version(['css/vendor.css', 'js/vendor.js'])
+       .less('main.less')
+       .version(['css/main.css', 'css/vendor.css', 'js/vendor.js'])
        .routes()
        .events();
 });
