04cef1e9ac829fd22bcca470e8b4cc35e775e5ab
peaceman <nixalio@gmail.com>
2014-10-27 10:06:05 +0100

    add redirection from home to the events index

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">nTimeRec</a>
+            <a class="navbar-brand" href="">nTimeRec</a>
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
