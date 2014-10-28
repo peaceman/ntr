f1ff4a43f914d94a07c616114ebc5719404f7f69
peaceman <nixalio@gmail.com>
2014-10-28 07:44:27 +0100

    bugfix: invalid form action

--- resources/views/events/col-ended-at.blade.php
+++ resources/views/events/col-ended-at.blade.php
@@ -1,7 +1,7 @@
 @if($event->hasEnded())
     {{ $event->ended_at }}
 @else
-    {!! Form::open(['route' => 'events.stop', $event->id]) !!}
+    {!! Form::open(['route' => ['events.stop', $event->id]]) !!}
     <input type="submit" value="Stop tracking" class="btn btn-danger btn-xs" />
     {!! Form::close() !!}
 @endif
