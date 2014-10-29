3223df7949289ed69b58a5615a7f723af5eee074
peaceman <nixalio@gmail.com>
2014-10-29 05:51:20 +0100

    install daterangepicker; copy fonts into public/build directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
+var gulp = require('gulp');
+var mainBowerFiles = require('main-bower-files');
 var elixir = require('laravel-elixir');
+var filter = require('gulp-filter');
+var notify = require('gulp-notify');
 require('laravel-elixir-bower');
 
-elixir(function(mix) {
-    mix.bower()
-       .less('main.less')
-       .version(['css/main.css', 'css/vendor.css', 'js/vendor.js'])
-       .routes()
-       .events();
+gulp.task('bower-fonts', function () {
+	return gulp.src(mainBowerFiles())
+		.pipe(filter(['**/*.eot', '**/*.woff', '**/*.ttf']))
+		.pipe(gulp.dest('public/build/fonts'))
+		.pipe(notify({
+			title: 'copied fonts from bower packages'
+		}));
+});
+
+elixir(function (mix) {
+	mix.bower()
+		.less('main.less')
+		.version(['css/main.css', 'css/vendor.css', 'js/vendor.js'])
+		.queueTask('bower-fonts')
+		.routes()
+		.events();
 });
